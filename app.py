2fc8f9da1057105c0b204080ee4d5ab319c7c033
Manvendra Singh <manvendra0310@gmail.com>
2016-11-26 19:18:40 +0530

    removed @run endpoint

--- app.py
+++ app.py
@@ -79,19 +79,6 @@ def create():
 	client.close()
 
 	return "DO Created & ssh tested"
-'''
-@app.run('/run')
-def run():
-	# Need SSH Key in req.args
-	key = paramiko.RSAKey(data=base64.decodestring('AAA...'))
-	client = paramiko.SSHClient()
-	client.get_host_keys().add('ssh.example.com', 'ssh-rsa', key)
-	client.connect('ssh.example.com', username='strongbad', password='thecheat')
-	stdin, stdout, stderr = client.exec_command('ls')
-	for line in stdout:
-		print ('... ' + line.strip('\n'))
-	client.close()
-"""
 
 @app.route('/')
 def index():
